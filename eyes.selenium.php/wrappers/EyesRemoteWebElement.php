e8db8f0b3f94d62f8af2dd473905d9372fd0631f
Itai Bar-Haim <itai.barhaim@applitools.com>
2017-10-11 09:20:39 +0300

    selenium - fixed javascript code and execution in EyesRemoteWebElement.

--- eyes.selenium.php/wrappers/EyesRemoteWebElement.php
+++ eyes.selenium.php/wrappers/EyesRemoteWebElement.php
@@ -34,16 +34,15 @@ class EyesRemoteWebElement extends RemoteWebElement
     private $executeMethod;
 
     const JS_GET_COMPUTED_STYLE_FORMATTED_STR =
-        " return arguments;"/* "var elem = arguments[0];" .
+         "var elem = arguments[0];" .
          "var styleProp = '%s';" .
          "if (window.getComputedStyle) {" .
-             "return window.getComputedStyle(arguments, null).getPropertyValue(styleProp);" .
+             "return window.getComputedStyle(elem, null).getPropertyValue(styleProp);" .
          "} else if (elem.currentStyle) {" .
          "   return elem.currentStyle[styleProp];" .
          "} else {" .
              "return null;" .
-         "}"*/
-    ;
+         "}";
 
     const JS_GET_SCROLL_LEFT = "return arguments[0].scrollLeft;";
     const JS_GET_SCROLL_TOP = "return arguments[0].scrollTop;";
@@ -93,16 +92,16 @@ class EyesRemoteWebElement extends RemoteWebElement
         $rect = $this->eyesDriver->executeScript(self::JS_GET_BOUNDING_CLIENT_RECT, array($this));
         return Region::CreateFromLTWH($rect['left'], $rect['top'], $rect['width'], $rect['height']);
     }
-
-    /*
+/*
     public function getBounds()
     {
         $loc = $this->webElement->getLocation();
         $size = $this->webElement->getSize();
-        return Region::CreateFromLTWH($loc->getX(), $loc->getY(), $size->getWidth(), $size->getHeight());
+        $region = Region::CreateFromLTWH($loc->getX(), $loc->getY(), $size->getWidth(), $size->getHeight());
+        $region->setCoordinatesType(CoordinatesType::CONTEXT_RELATIVE);
+        return $region;
     }
-    */
-
+*/
     public function getClientAreaBounds(){
         $bounds = $this->getBounds();
 
@@ -132,8 +131,7 @@ class EyesRemoteWebElement extends RemoteWebElement
     {
         $scriptToExec = sprintf
         (self::JS_GET_COMPUTED_STYLE_FORMATTED_STR, $propStyle);
-        return $this->eyesDriver->getRemoteWebDriver()->executeScript($scriptToExec);
-        // return $this->eyesDriver->getRemoteWebDriver()->execute($scriptToExec);
+        return $this->eyesDriver->executeScript($scriptToExec, [$this]);
     }
 
     /**
