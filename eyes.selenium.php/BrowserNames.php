9512bf279233fe5180cd43459acd5e2344910017
Itai Bar-Haim <itai.barhaim@applitools.com>
2017-10-15 15:34:42 +0300

    selenium - added browser names and OSs names constants.

---
+++ eyes.selenium.php/BrowserNames.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Applitools\Selenium;
+
+class BrowserNames
+{
+    const Edge = "Edge";
+    const IE = "IE";
+    const Firefox = "Firefox";
+    const Chrome = "Chrome";
+    const Safari = "Safari";
+    const Chromium = "Chromium";
+}
---
+++ eyes.selenium.php/OSNames.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Applitools\Selenium;
+
+class OSNames
+{
+    const Unknown = "Unknown";
+    const Windows = "Windows";
+    const IOS = "IOS";
+    const Macintosh = "Macintosh";
+    const ChromeOS = "ChromeOS";
+}
