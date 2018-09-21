596cd4b2a0cce20b283c4d52ee1721e6243bc1b6
Michael Yosher <michael.yosher@applitools.com>
2018-09-21 09:52:12 +0300

    Fix that saves the original page size for trimming the black background at the end of method.

--- eyes.selenium.php/capture/FullPageCaptureAlgorithm.php
+++ eyes.selenium.php/capture/FullPageCaptureAlgorithm.php
@@ -160,6 +160,7 @@ class FullPageCaptureAlgorithm
         $imageParts = $entirePage->getSubRegions($partImageSize);
         //$this->logger->verbose("imageParts: " . var_export($imageParts, true));
 
+        $originalEntirePage = $entireSize;
         $this->logger->verbose("pixelRatio: $pixelRatio");
         $entireSize = $entireSize->scale($pixelRatio);
 
@@ -242,7 +243,7 @@ class FullPageCaptureAlgorithm
 
         if ($actualImageWidth < $entireSize->getWidth() || $actualImageHeight < $entireSize->getHeight()) {
             $this->logger->verbose("Trimming unnecessary margins...");
-            $stitchedImage = ImageUtils::getImagePart($stitchedImage, Region::CreateFromLTWH(0, 0, $actualImageWidth, $actualImageHeight));
+            $stitchedImage = ImageUtils::getImagePart($stitchedImage, Region::CreateFromLTWH(0, 0, $originalEntirePage->getWidth(), $originalEntirePage->getHeight()));
             $this->logger->verbose("Done!");
         }
         $debugScreenshotsProvider->save($stitchedImage, "stitched");
