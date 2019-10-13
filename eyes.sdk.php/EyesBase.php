5229989c491631c28bb926666c4c4f790f5aa476
Daniel Puterman <daniel.puterman@applitools.com>
2019-10-14 00:21:49 +0300

    Updated default eyes server to eyesapi.applitools.com

--- eyes.sdk.php/EyesBase.php
+++ eyes.sdk.php/EyesBase.php
@@ -676,7 +676,7 @@ abstract class EyesBase
 
     public static function getDefaultServerUrl()
     {
-        return "https://eyessdk.applitools.com";
+        return "https://eyesapi.applitools.com";
     }
 
     /** Superseded by {@link #setHostOS(String)} and {@link #setHostApp (String)}.
